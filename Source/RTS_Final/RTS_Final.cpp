372d4a8f52b95a89b359a39fb31a4c7f890a19ba
Pavel <pedaracha@gmail.com>
2025-08-30 19:22:53 +0300

    Initial commit

---
+++ Source/RTS_Final/RTS_Final.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RTS_Final.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTS_Final, "RTS_Final" );
---
+++ Source/RTS_Final/RTS_FinalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RTS_FinalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_FINAL_API ARTS_FinalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
